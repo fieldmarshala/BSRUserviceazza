370a7bea7b66d33ce1af12a6d3ed38b6fe13d854
udomsak <fieldmarshala@gmail.com>
2019-03-04 14:51:34 +0700

    Register Success

---
+++ app/src/main/java/udomsak/kawsodsee/ac/th/bsruserviceazza/MyConstant.java
@@ -0,0 +1,10 @@
+package udomsak.kawsodsee.ac.th.bsruserviceazza;
+
+public class MyConstant {
+
+    private String urlAddUser = "https://www.androidthai.in.th/bsru/addUserA.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
